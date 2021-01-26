38fe96bf20ec94889b09563224d4c01225c29534
mzz2017 <m@mzz.pub>
2021-01-26 19:43:34 +0800

    limit rate

--- app.js
+++ app.js
@@ -366,7 +366,7 @@ async function musicCallback(msg, match) {
             let needOtherSource = true
             await check_music({id: song.id}).then(async (resp) => {
                 if (resp.body.success) {
-                    await song_url({id: song.id, /*br: 320000,*/ cookie}).then((res) => {
+                    await song_url({id: song.id, br: 320000, cookie}).then((res) => {
                         const {body: {data: [{url: url, freeTrialInfo: freeTrialInfo}]}} = res
                         if (freeTrialInfo) {
                             return
